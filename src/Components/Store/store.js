7cb20edb52d7ce8870cfd5590fca32aaa364b842
Kapadam <dharanidharu211@gmail.com>
2023-02-07 11:53:29 +0530

    created

---
+++ src/Components/Store/store.js
@@ -0,0 +1,7 @@
+import {configureStore} from '@reduxjs/toolkit';
+import studentReducer from '../studentSlice';
+export default configureStore({
+    reducer:{
+        student:studentReducer
+    }
+})
